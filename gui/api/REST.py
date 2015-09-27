6cf07f04eefbd22dbf4fbf38a5f519a36386253b
Rogers <trogers1@ccb.tbc.iit.edu>
2015-09-27 17:50:39 -0500

    Changes to add protocol

--- gui/api/REST.py
+++ gui/api/REST.py
@@ -7,9 +7,14 @@ app = Flask(__name__)
 
 try:
     TARGET = os.environ['TARGET']
+    AlGDock = os.environ['AlGDock']
 except Exception:
     print 'export TARGET=<path to data>'
     exit(1)
+
+import sys
+sys.path.insert(0, AlGDock)
+from BindingPMF_arguments import *
 
 @app.route('/api/v1.0/proteins', methods=['GET', 'OPTIONS'])
 @crossdomain(origin='*')
@@ -24,6 +29,13 @@ def get_ligand_names(protein):
     ligands = os.walk(os.path.join(TARGET, protein, "ligand")).next()[2]
     ligand_lst = [{"filename": ligand} for ligand in sorted(ligands)]
     return jsonify({"files": ligand_lst})
+
+@app.route('/api/v1.0/protocols', methods=['GET', 'OPTIONS'])
+@crossdomain(origin='*')
+def get_protocols(parameter):
+    choices = arguments['protocol']['choices']
+    choice_lst = [{"choice": choice} for choice in choices]
+    return jsonify({"protocol": choice_lst})
 
 @app.route('/api/v1.0/run/<protein>/<ligand>', methods=['GET', 'OPTIONS'])
 @crossdomain(origin='*')
