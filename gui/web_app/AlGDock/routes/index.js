688f71c0aea3825fedfc35414214f0aebc77c4ab
morlowsk <morlowsk@hawk.iit.edu>
2015-10-11 18:32:03 -0500

    send email upon registering

--- gui/web_app/AlGDock/routes/index.js
+++ gui/web_app/AlGDock/routes/index.js
@@ -55,59 +55,57 @@ router.get('/', valid_user, function(req, res, next) {
 // Login and Registration
 router.post('/', function(req, res) {
     mongoose.model('peoples').findOne({people_username : req.body.login_mail}, function(err, peoples) {
-	if (peoples === null) {
-	    console.log(err);
-	    res.render('login', { hide_class : '', alert_type: 'alert-danger', alert: "We're sorry. We could not find a user with those credentials."});
-	}
 
-	else if (create_hash(req.body.login_pass) === peoples["people_password"]){
-	    if (peoples["last_login"] === null){
-		peoples["email_verify"] = generate_tok();
-		db_save(peoples);
-
-		verify_link = 'http://localhost:3000/verify_email/' + req.body.login_mail + '/' + peoples["email_verify"];
-
-		var verifyEmail = {
-		    from: 'AlGDock Admin <algdock.ipro@gmail.com>',
-		    to: peoples["people_username"],
-		    subject: 'AlGDock | Email Verification',
-		    html: 'You are receiving this message because you must verify your email address.<br><br>Just click the link below and you are done!<br>' + '<a href=\"' + verify_link + '\">' + verify_link + '</a>'
-		};
-
-		send_mail(verifyEmail);
-		res.render('login', {hide_class : '', alert_type: 'alert-success', alert : "Please verify your email address. An email has been sent to you." });
-	    }
-	    else {
-		logged_tok = generate_tok();
-
-		res.cookie('logged_tok', logged_tok);
-		res.cookie('user', peoples["people_username"]);
-		peoples["logged_tok"] = logged_tok;
-		db_save(peoples);
-		res.redirect('/');
-	    }
-	}
-	else {
-	    res.render('login', { hide_class : '', alert_type: 'alert-danger', alert: 'Invalid log in.'});
-	}
+		if (peoples === null) {
+			console.log(err);
+			res.render('login', { hide_class : '', alert_type: 'alert-danger', alert: "We're sorry. We could not find a user with those credentials."});
+		}
+		else if (create_hash(req.body.login_pass) === peoples["people_password"]){
+			logged_tok = generate_tok();
+			res.cookie('logged_tok', logged_tok);
+			res.cookie('user', peoples["people_username"]);
+			peoples["logged_tok"] = logged_tok;
+			db_save(peoples);
+			res.redirect('/');
+		}
+		else {
+			res.render('login', { hide_class : '', alert_type: 'alert-danger', alert: 'Invalid log in.'});
+		}
     });
 });
 
 router.post('/reg', function(req, res, next) {
     var email = req.body.reg_mail;
     mongoose.model('peoples').findOne({people_username : email}, function(err, peoples){
-	if(peoples !== null){
-	    res.render('login', {hide_class : '', alert_type: 'alert-danger', alert: 'This email is already in use. Did you forget your password?'});
-	}
-	else{
-	    var new_people = new People({
-		people_username : email,
-		people_password : create_hash(req.body.reg_pass2),
-	    });
-	    db_save(new_people);
-	    res.render('login', {hide_class : '', alert_type: 'alert-success', alert: 'You have been successfully registered.'});
-	}
-    });
+		if(peoples !== null) {
+			res.render('login', {hide_class : '', alert_type: 'alert-danger', alert: 'This email is already in use. Did you forget your password?'});
+		}
+		else {
+			var new_people = new People({
+			people_username : email,
+			people_password : create_hash(req.body.reg_pass2),
+			});
+			db_save(new_people);
+			
+			mongoose.model('peoples').findOne({people_username : email}, function(err, peoples) {
+
+				peoples["email_verify"] = generate_tok();
+				db_save(peoples);
+
+				verify_link = 'http://localhost:3000/verify_email/' + email + '/' + peoples["email_verify"];
+ 
+				var verifyEmail = {
+					from: 'AlGDock Admin <algdock.ipro@gmail.com>',
+					to: peoples["people_username"],
+					subject: 'AlGDock | Email Verification',
+					html: 'You are receiving this message because you must verify your email address.<br><br>Just click the link below and you are done!<br>' + '<a href=\"' + verify_link + '\">' + verify_link + '</a>'
+				};
+
+				send_mail(verifyEmail);
+				res.render('login', {hide_class : '', alert_type: 'alert-success', alert : "You have been successfully registered, please verify your email address. An email has been sent to you." });
+			});	
+		}
+	});
 });
 
 router.get('/verify_email/:email/:tok', function(req, res) {
