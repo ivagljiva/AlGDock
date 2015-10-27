f1759ef393cfd3fae88733fdd1fd1b988e9fe605
Rogers <trogers1@ccb.tbc.iit.edu>
2015-10-27 00:24:36 -0500

    Added reps to index page

--- gui/api/create_preferences.sh
+++ gui/api/create_preferences.sh
@@ -29,35 +29,34 @@ cat << EOF
 import AlGDock.BindingPMF_plots
 import os, shutil, glob
 
-for run_type in ['$RUN_TYPE']:
-  self = AlGDock.BindingPMF_plots.BPMF_plots(
-    dir_dock='dock', dir_cool='cool',
-    ligand_tarball='prmtopcrd/ligand.tar.gz',
-    ligand_database='ligand.db',
-    forcefield='prmtopcrd/gaff.dat',
-    ligand_prmtop='ligand.prmtop',
-    ligand_inpcrd='ligand.trans.inpcrd',
-    receptor_tarball='prmtopcrd/receptor.tar.gz',
-    receptor_prmtop='receptor.prmtop',
-    receptor_inpcrd='receptor.trans.inpcrd', 
-    receptor_fixed_atoms='receptor.pdb', 
-    complex_tarball='prmtopcrd/complex.tar.gz', 
-    complex_prmtop='complex.prmtop', 
-    complex_inpcrd='complex.trans.inpcrd', 
-    complex_fixed_atoms='complex.pdb', 
-    score = 'prmtopcrd/anchor_and_grow_scored.mol2', 
-    dir_grid='grids', 
-    protocol='$PROTOCOL', cool_therm_speed=$COOL_THERM_SPEED, dock_therm_speed=$DOCK_THERM_SPEED,
-    sampler='$SAMPLER', 
-    MCMC_moves=$MCMC_MOVES, 
-    seeds_per_state=$SEEDS_PER_STATE, steps_per_seed=$STEPS_PER_SEED,
-    sweeps_per_cycle=$SWEEPS_PER_CYCLE, attempts_per_sweep=$ATTEMPTS_PER_SWEEP, steps_per_sweep=$STEPS_PER_SWEEP,
-    cool_repX_cycles=$COOL_REPX_CYCLES, dock_repX_cycles=$DOCK_REPX_CYCLES, 
-    site='$SITE', site_center=[$X, $Y, $Z], site_max_R=$SITE_MAX_R, 
-    site_density=$SITE_DENSITY, 
-    phases=['$PHASES'], 
-    cores=$CORES, 
-    rmsd=$RMSD)
-  self._run(run_type)
-  del self
+self = AlGDock.BindingPMF_plots.BPMF_plots(
+  dir_dock='dock', dir_cool='cool',
+  ligand_tarball='prmtopcrd/ligand.tar.gz',
+  ligand_database='ligand.db',
+  forcefield='prmtopcrd/gaff.dat',
+  ligand_prmtop='ligand.prmtop',
+  ligand_inpcrd='ligand.trans.inpcrd',
+  receptor_tarball='prmtopcrd/receptor.tar.gz',
+  receptor_prmtop='receptor.prmtop',
+  receptor_inpcrd='receptor.trans.inpcrd', 
+  receptor_fixed_atoms='receptor.pdb', 
+  complex_tarball='prmtopcrd/complex.tar.gz', 
+  complex_prmtop='complex.prmtop', 
+  complex_inpcrd='complex.trans.inpcrd', 
+  complex_fixed_atoms='complex.pdb', 
+  score = 'prmtopcrd/anchor_and_grow_scored.mol2', 
+  dir_grid='grids', 
+  protocol='$PROTOCOL', cool_therm_speed=$COOL_THERM_SPEED, dock_therm_speed=$DOCK_THERM_SPEED,
+  sampler='$SAMPLER', 
+  MCMC_moves=$MCMC_MOVES, 
+  seeds_per_state=$SEEDS_PER_STATE, steps_per_seed=$STEPS_PER_SEED,
+  sweeps_per_cycle=$SWEEPS_PER_CYCLE, attempts_per_sweep=$ATTEMPTS_PER_SWEEP, steps_per_sweep=$STEPS_PER_SWEEP,
+  cool_repX_cycles=$COOL_REPX_CYCLES, dock_repX_cycles=$DOCK_REPX_CYCLES, 
+  site='$SITE', site_center=[$X, $Y, $Z], site_max_R=$SITE_MAX_R, 
+  site_density=$SITE_DENSITY, 
+  phases=['$PHASES'], 
+  cores=$CORES, 
+  rmsd=$RMSD)
+self._run($RUN_TYPE)
+del self
 EOF
