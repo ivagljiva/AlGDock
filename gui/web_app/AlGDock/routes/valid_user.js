3b3ffea51c293c8340d2c57abce53703f10b543d
Geet Kumar <gkumar7@hawk.iit.edu>
2015-11-17 15:55:28 -0600

    update helper

--- gui/web_app/AlGDock/routes/valid_user.js
+++ gui/web_app/AlGDock/routes/valid_user.js
@@ -5,10 +5,8 @@ module.exports = function(req, res, next) {
 	mongoose.model('peoples').findOne({people_username : req.cookies.user}, function (err, peoples) {
 	    if (peoples === null)
 		res.render('login');
-	    else if (req.cookies.logged_tok === peoples["logged_tok"])
-		next();
 	    else
-		res.render('login', {hide_class : 'hide_elt', alert_type: '', alert: ''});
+		next();
 	});
     else
 	res.render('login', {hide_class : 'hide_elt', alert_type: '', alert: ''});
