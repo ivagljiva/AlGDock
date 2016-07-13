9fa174d92ee01c94ff310a85fc031dc9a051ab90
Iva Veseli <iveseli@hawk.iit.edu>
2016-07-13 09:05:38 -0500

    testing command line parsing for prep script

--- gui/protein_prep.sh
+++ gui/protein_prep.sh
@@ -2,6 +2,21 @@
 
 # Attempted automation of pipeline.txt to prepare protein files for docking
 
-echo $TARGET;	# double check that this is set to the correct directory
+echo $TARGET;	# double check that this is set to the correct directory: /home/ldasilva/target/
 
-
+# parse command line argument to get protein sequence file
+for i in "$@"
+do
+case $i in
+	-p=*|--protein=*)	# protein sequence file should go after =
+	SEQUENCE="${i#*=}"
+	echo 'Sequence file is $SEQUENCE';
+	shift	# go to next argument
+	;;
+	# you can add more arguments here in same format
+	*)
+	# unknown argument
+	echo 'Argument $i is not known';
+esac #end case statement
+shift
+done
